44cd0ad4638a2e28c03fbd9ef8cce1d63c561b6d
juliosm3 <tuemailexample.com>
2024-12-09 16:52:23 +0100

    modificar archivos

---
+++ js/reloj.js
@@ -0,0 +1,7 @@
+const index = document.getElementById('index')
+const actualizarHora = () => {
+    const ahora = new Date();
+    const horas = ahora.getHours();
+    const minutos = ahora.getMinutes();
+    const segundos = ahora.getSeconds();
+}
